3fb48a420cab628159aad6a919d0b10c2de797da
仕林林 <295755973@qq.com>
2022-04-23 11:38:16 +0800

    🐞 fix: config

--- mumu.config.js
+++ mumu.config.js
@@ -1,8 +1,6 @@
-const config = {
+module.exports = {
   webDomian: 'https://mumu-page.github.io/mumu-global-component',
   gitUrl: 'git@github.com:mumu-page/mumu-global-component.git',
   name: 'mumu-global-component',
   description: "远程全局组件"
 }
-
-export default config
